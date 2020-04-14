e3a1e9ab56ae741cca7d65d8cf1d0837e05e246b
Kao <kaworinakano@gmail.com>
2020-04-15 05:44:58 +0800

    Initial release of image unscrambler

---
+++ ImageUnscrambler/ImageSplitter.cs
@@ -0,0 +1,64 @@
+﻿using System.Drawing;
+
+namespace ImageUnscrambler
+{
+    public static class ImageSplitter
+    {
+        public static Image[][] SplitImage(this Image image, int rows, int? columns = null)
+        {
+            var col = columns ?? rows;
+
+            var images = new Image[rows][];
+            var bmp = new Bitmap(image);
+
+            var width = image.Width / col;
+            var height = image.Height / rows;
+
+            for (var y = 0; y < rows; y++)
+            {
+                images[y] = new Image[col];
+                for (var x = 0; x < col; x++)
+                {
+                    var clone = bmp.Clone(new Rectangle(x * width, y * height, width, height), bmp.PixelFormat);
+                    images[y][x] = clone;
+                }
+            }
+
+            return images;
+        }
+
+        public static Image[][] InvertAxis(this Image[][] images)
+        {
+            var ret = new Image[images.Length][];
+
+            for (var y = 0; y < images.Length; y++)
+            {
+                ret[y] = new Image[images[y].Length];
+                for (var x = 0; x < images[y].Length; x++) ret[y][x] = images[x][y];
+            }
+
+            return ret;
+        }
+
+        public static Image CombineImages(this Image[][] images, int width, int height)
+        {
+            var bitmap = new Bitmap(width, height);
+            using (var g = Graphics.FromImage(bitmap))
+            {
+                for (var y = 0; y < images.Length; y++)
+                {
+                    for (var x = 0; x < images[y].Length; x++)
+                    {
+                        g.DrawImage(images[y][x],
+                            images[y][x].Width * x,
+                            images[y][x].Height * y);
+                    }
+                }
+
+                g.Save();
+            }
+
+            return bitmap;
+        }
+    }
+}
---
+++ ImageUnscrambler/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.IO.Enumeration;
+
+namespace ImageUnscrambler
+{
+    internal class Program
+    {
+        private static Point Point(int x, int y)
+        {
+            return new Point(x, y);
+        }
+
+        private static void Main(string[] args)
+        {
+            while(true)
+            {
+                var rows = Prompt<int>("Enter rows", int.TryParse);
+                var cols = Prompt<int>("Enter columns", int.TryParse);
+
+                var path = Prompt("Enter path");
+                if (Directory.Exists(path))
+                {
+                    var isRecursive = Prompt<bool>("Recursive?", bool.TryParse);
+                    var images = GetImagesFromFolder(path, isRecursive);
+                    foreach (var image in images) UnscrambleImage(rows, cols, image);
+                }
+                else if (File.Exists(path))
+                    UnscrambleImage(rows, cols, path);
+                else
+                    Console.WriteLine("No image was found.");
+            }
+        }
+
+        private static void SaveImage(Image result, string path)
+        {
+            var fileName = Path.Combine(
+                Path.GetDirectoryName(path) ?? string.Empty,
+                $"{Path.GetFileNameWithoutExtension(path)}_reversed.png");
+            result.Save(fileName, ImageFormat.Png);
+        }
+
+        private static void UnscrambleImage(int rows, int cols, string path)
+        {
+            var image = Image.FromFile(path);
+            image = image
+                .SplitImage(rows, cols)
+                .InvertAxis()
+                .CombineImages(image.Width, image.Height);
+
+            SaveImage(image, path);
+        }
+
+        private static IEnumerable<string> GetImagesFromFolder(string path, bool isRecursive = false)
+        {
+            var options = new EnumerationOptions {RecurseSubdirectories = isRecursive};
+            return new FileSystemEnumerable<string>(path, ToImage, options)
+            {
+                ShouldIncludePredicate = IsImage
+            };
+
+            static string ToImage(ref FileSystemEntry entry)
+            {
+                return entry.ToFullPath();
+            }
+
+            static bool IsImage(ref FileSystemEntry entry)
+            {
+                return entry.FileName.EndsWith(".png") || entry.FileName.EndsWith(".jpg") ||
+                       entry.FileName.EndsWith(".jpeg");
+            }
+        }
+
+        private static string Prompt(string question)
+        {
+            Console.Write($"{question} > ");
+            return Console.ReadLine();
+        }
+
+        private static T Prompt<T>(string question, TryParseEnum<T> tryParse)
+        {
+            while (true)
+                if (tryParse(Prompt(question), out var result))
+                    return result;
+        }
+
+        private delegate bool TryParseEnum<T>(string input, out T result);
+    }
+}
